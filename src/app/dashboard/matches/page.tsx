9dad2de856abdf5fcc5453cc1bab5b6258bf3635
Robin-123456756 <wambijames7@gmail.com>
2025-12-22 15:49:14 +0300

    Remove unnecessary DropdownMenu closing tag

--- src/app/dashboard/matches/page.tsx
+++ src/app/dashboard/matches/page.tsx
@@ -232,7 +232,6 @@ export default function MatchesPage() {
                   </DropdownMenuItem>
                 ))}
               </DropdownMenuContent>
-            </DropdownMenu>
           </div>
         </div>
 
