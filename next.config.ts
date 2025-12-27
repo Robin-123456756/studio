d8ac350fdfb0485cabc9178d5f75cf59b87e8f33
picklerick1309 <philip.nsimbe@maximusglobal.net>
2025-12-28 02:53:26 +0300

    changes committed

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 const withPWA = require("next-pwa")({
   dest: "public",
+  register: true,
+  skipWaiting: true,
+  clientsClaim: true,
   disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "picsum.photos" }
-    ],
-  },
+  // your existing next config...
 });
